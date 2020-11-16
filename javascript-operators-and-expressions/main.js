b1a45dbcdc370f599bbc5948d9062e18cfb4b0bd
udaibirv <udaibirv@gmail.com>
2020-11-16 15:27:23 -0800

    completed javascript-operators-and-expressions exercise

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-console */
+var width = 5;
+var height = 6;
+var area = width * height;
+
+console.log('The value of area is: ' + area);
+console.log('The typeof of area is: ' + typeof area);
+
+var bill = 499;
+var payment = 250;
+var change = bill - payment;
+
+console.log('The value of change is: ' + change);
+console.log('The typeof of change is: ' + typeof change);
+
+var quizzes = 85;
+var midterm = 92;
+var final = 90;
+var grade = (quizzes + midterm + final) / 3;
+
+console.log('The value of grade is: ' + grade);
+console.log('The typeof grade is: ' + typeof grade);
+
+var firstName = 'Udaibir';
+var lastName = 'Virk';
+var fullName = firstName + ' ' + lastName;
+
+console.log('The value of fullName is: ' + fullName);
+console.log('The typeof fullName is: ' + typeof fullName)
+
+var ph = 8;
+var isAcidic = ph < 7;
+
+console.log('The value of isAcidic is: ' + isAcidic);
+console.log('The typeof isAcidic is: ' + typeof isAcidic);
+
+var headCount = 450;
+var isSparta = (headCount === 300);
+
+console.log('The value of isSparta is: ' + isSparta);
+console.log('The typeof isSparta is: ' + typeof isSparta);
+
+var motto = fullName + ' is the GOAT';
+console.log('The value of motto is: ' + motto);
+console.log('The typeof motto is: ' + typeof motto);
