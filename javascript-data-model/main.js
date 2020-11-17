dc64662938ed248a3d0ca9bf20d40aa9cee0115f
udaibirv <udaibirv@gmail.com>
2020-11-17 11:11:52 -0800

    completed javascript-data-model exercise

---
+++ javascript-data-model/main.js
@@ -0,0 +1,73 @@
+/* exported orderHistory */
+
+var orderHistory = [
+  {
+    orderPlaced: 'August 4, 2020',
+    total: 34,
+    shipTo: 'JS Masher',
+    orderNumber: '#114-3941689-8772232',
+    deliveredOn: 'August 8, 2020'
+    orderType: [
+      {
+        type: 'Book',
+        title: 'JavaScript for impatient programmers',
+        author: 'Rauschmayer, Dr. Axel',
+        returnWindow: 'Return window closed on September 7, 2020',
+        price: 31.55
+      }
+    ]
+  },
+  {
+    orderPlaced: 'July 19, 2020',
+    total: 44.53,
+    shipTo: 'JS Masher',
+    orderNumber: '#113-9984268-1280257',
+    deliveredOn: 'July 20, 2020',
+    orderType: [
+      {
+        type: 'Book',
+        title: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        returnWindow: 'Return window closed on August 19, 2020',
+        price: 41.33
+      }
+    ]
+  },
+  {
+    orderPlaced: 'July 4, 2020',
+    total: 17.22,
+    shipTo: 'JS Masher',
+    orderNumber: '#114-2875557-905409',
+    deliveredOn: 'July 7, 2020',
+    orderType: [
+      {
+        type: 'Electronics',
+        title: 'Gamecube Controller Adapater. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adaper',
+        returnWindow: 'Return window closed on August 5, 2020',
+        price: 15.98
+      }
+    ]
+  },
+  {
+    orderPlaced: 'July 3, 2020',
+    total: 138.93,
+    shipTo: 'JS Masher',
+    orderNumber: '#113-2883177-2648248',
+    deliveredOn: 'July 5, 2020',
+    orderType: [
+      {
+        type: 'Electronics',
+        title: 'GameCub Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        returnWindow: 'Return window closed on August 4, 2020',
+        price: 94.95
+      },
+      {
+        type: 'Book',
+        title: 'Faroult, Stephane',
+        returnWindow: 'Return window closed on August 4, 2020',
+        price: 33.99
+      }
+    ]
+  }
+
+];
