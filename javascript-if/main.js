ff34adbce615f8b542a1b0b9e04e2e80e7662987
udaibirv <udaibirv@gmail.com>
2020-11-18 12:50:17 -0800

    completed javascript-if exercise

---
+++ javascript-if/main.js
@@ -0,0 +1,77 @@
+/* exported isUnderFive,
+            isEven,
+            startsWithJ,
+            isOldEnoughToDrink,
+            isOldEnoughToDrive,
+            isOldEnoughToDrinkAndDrive,
+            categorizeAcidity,
+            introduceWarnerBro
+ */
+
+function isUnderFive(number){
+  if(number < 5){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+function isEven(number){
+  if(number % 2 === 0){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+function startsWithJ(string){
+  if(string.charAt(0) === 'J'){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+function isOldEnoughToDrink(person){
+  if(person.age <= 21){
+    return false;
+  }else{
+    return true;
+  }
+}
+
+function isOldEnoughToDrive(person){
+  if(person.age >= 16){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+function isOldEnoughToDrinkAndDrive(person){
+  if(person){
+    return false;
+  }
+}
+
+function categorizeAcidity(pH){
+  if(pH < 7){
+    return 'Acid';
+  }else if(pH === 7){
+    return 'Nuetral';
+  }else if(pH > 7 && pH <=14){
+    return 'Base';
+  }else{
+    return 'Invalid pH Level';
+  }
+}
+
+function introduceWarnerBro(name){
+  if(name === 'yakko' || name === 'wakko'){
+    return 'We\'re the warner brothers!';
+  }else if(name === 'dot'){
+    return 'I\'m cute~';
+  }else{
+    return 'Goodnight everybody!';
+  }
+}
