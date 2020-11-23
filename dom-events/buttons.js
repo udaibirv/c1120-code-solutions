e9b0c8b35e3e3b39a6f15bf090540306e897c716
udaibirv <udaibirv@gmail.com>
2020-11-23 12:54:13 -0800

    completed dom-events exercise

---
+++ dom-events/buttons.js
@@ -0,0 +1,24 @@
+function handleClick(event){
+  console.log('button clicked');
+  console.log(event);
+  console.log(event.target);
+}
+var $clickButton = document.querySelector('.click-button');
+$clickButton.addEventListener('click', handleClick);
+
+
+function handleMouseover(event){
+  console.log('button hovered');
+  console.log(event);
+  console.log(event.target);
+}
+var $hoverButton = document.querySelector('.hover-button');
+$hoverButton = document.addEventListener('mouseover', handleMouseover);
+
+function handleDoubleClick(event){
+  console.log('button double-clicked');
+  console.log(event);
+  console.log(event.target);
+}
+$doubleButton = document.querySelector('.double-click-button');
+$doubleButton = document.addEventListener('dblclick', handleDoubleClick);
