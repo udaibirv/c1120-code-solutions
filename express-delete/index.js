0d404c2b1f72aece5dd98fab61eadfac2e993593
udaibirv <udaibirv@gmail.com>
2021-01-06 19:19:44 -0800

    completed express-delete exercise

---
+++ express-delete/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92
+  }
+}
+
+const app = express();
+app.get('/api/grades', function(req, res){
+  const gradesArray = [];
+  for(const id in grades){
+    gradesArray.push(grades[id]);
+  }
+  res.json(gradesArray);
+})
+
+app.delete('/api/grades/:id', function(req, res){
+  const id = parseInt(req.params.id);
+  if (!grades[id]) {
+    res.sendStatus(404);
+  }
+  delete grades[id];
+  res.sendStatus(204);
+})
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+})
