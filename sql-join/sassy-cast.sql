238872df3ff27cdd2ce36e1e631612416d52dc4b
udaibirv <udaibirv@gmail.com>
2021-01-12 15:59:55 -0800

    completed sql-join exercise

---
+++ sql-join/sassy-cast.sql
@@ -0,0 +1,5 @@
+select "firstName", "lastName"
+  from "actors"
+  join "castMembers" using ("actorId")
+  join "films" using ("filmId")
+  where "films"."title" = 'Jersey Sassy';
---
+++ sql-join/what-and-when.sql
@@ -0,0 +1,5 @@
+select "releaseYear", "categories"."name" as "category"
+  from "films"
+  join "filmCategory" using ("filmId")
+  join "categories" using ("categoryId")
+  where "films"."title" = 'Boogie Amelie';
