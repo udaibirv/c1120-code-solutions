b3b2ca459758ecf50abc4eab951718202d33bb63
udaibirv <udaibirv@gmail.com>
2021-01-18 15:08:44 -0800

    completed react-function-components exercise

---
+++ react-function-components/src/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function CustomButton(){
+  return <button>Click me!</button>;
+}
+
+
+ReactDOM.render(CustomButton, document.getElementById('root'));
