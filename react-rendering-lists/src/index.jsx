b63b001c99684953e01f96113b0e8f1ef190c43d
udaibirv <udaibirv@gmail.com>
2021-01-20 14:48:03 -0800

    completed react-rendering-lists exercise

---
+++ react-rendering-lists/src/index.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const pokedex = [
+  { number: '001', name: 'Bulbasaur' },
+  { number: '004', name: 'Charmander' },
+  { number: '007', name: 'Squirtle' },
+  { number: '025', name: 'Pikachu' },
+  { number: '039', name: 'Jigglypuff' }
+];
+
+const element = (
+  <ul>
+    {
+      pokedex.map(pokemon => {
+        return <li key={pokemon.number}> {pokemon.name}</li>
+      })
+    }
+  </ul>
+)
+
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
