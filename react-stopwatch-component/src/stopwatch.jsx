ee1ba0faab016708e8dc2592882537bfa380fa6a
udaibirv <udaibirv@gmail.com>
2021-01-20 12:01:26 -0800

    work in progress

---
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+class Stopwatch extends React.Component{
+constructor(props){
+  super(props);
+  this.handleClick = this.handleClick.bind(this);
+  this.state = {isPlay: false};
+}
+
+handleClick(){
+  console.log('Click worked');
+  const isPlayState = this.state.isPlay;
+  this.setState({isPlay: !isPlayState});
+}
+
+}
+
+
+
+
+
+export default Stopwatch;
