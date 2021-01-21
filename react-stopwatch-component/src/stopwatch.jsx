a02a311480baa7270d7a0cc9c674751592fb89ac
udaibirv <udaibirv@gmail.com>
2021-01-20 20:39:03 -0800

    work in progress

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -3,15 +3,44 @@ import React from "react";
 class Stopwatch extends React.Component{
 constructor(props){
   super(props);
-  this.handleClick = this.handleClick.bind(this);
-  this.state = {isPlay: false};
+  this.incrementer = this.incrementer.bind(this);
+  this.pause = this.pause.bind(this);
+  this.reset = this.reset.bind(this);
+  this.state = {count: 0, intervalId: null };
 }
 
-handleClick(){
-  console.log('Click worked');
-  console.log("state is: ", isPlayState);
-  const isPlayState = this.state.isPlay;
-  this.setState({isPlay: !isPlayState});
+
+
+incrementer(){
+  const start = setInterval(() => {
+    this.setState({count: this.state.count + 1});
+  }, 1000)
+  this.setState({intervalId: start});
+}
+
+pause(){
+  clearInterval(this.state.intervalId);
+  this.setState({intervalId: null});
+
+}
+
+reset(){
+  if(this.state.count > 0 && this.state.intervalId === null){
+    const start = setInterval(() => {
+      this.setState({ count: 0 });
+    }, 1000)
+    this.setState({intervalId: start});
+  }
+}
+
+render(){
+  return (
+    <div className="wrapper">
+      <div onClick={this.reset} className="circle"> {this.state.count} </div>
+      <i onClick={this.incrementer} className="far fa-play-circle"></i>
+      <i onClick={this.pause}className="fas fa-pause"></i>
+    </div>
+  )
 }
 
 }
