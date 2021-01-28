a93bc79acbe1fbf0f3d38a3ccafa4e2d71e43dd4
udaibirv <udaibirv@gmail.com>
2021-01-27 20:10:36 -0800

    completed react-context exercise

---
+++ react-context/client/components/page-container.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const styles = {
+  page: {
+    minHeight: 'calc(100vh - 3.5rem)'
+  }
+};
+
+export default function PageContainer({ children }) {
+  return (
+    <div className="bg-light">
+      <div className="container" style={styles.page}>
+        { children }
+      </div>
+    </div>
+  );
+}
